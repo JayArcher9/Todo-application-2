8804fa4d2c8f050e95ca3a23605bbba251b89ff2
JayArcher9 <143174572+JayArcher9@users.noreply.github.com>
2023-12-23 17:48:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,35 @@ let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
 let inp = document.querySelector("input");
 
-btn.addEventListener("click", function() {
+function addItem() {
     let item = document.createElement("li");
     item.innerText = inp.value;
     ul.appendChild(item);
     inp.value = "";
+
+    let delbtn = document.createElement("button");
+    delbtn.innerText = "delete";
+    delbtn.classList.add("delete");
+
+    item.appendChild(delbtn);
+}
+
+
+
+btn.addEventListener("click", addItem);
+
+inp.addEventListener("keypress", function(event) {
+    // Check if the key pressed is Enter (key code 13)
+    if (event.key === "Enter") {
+        addItem();
+    }
+});
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let listitem = event.target.parentElement;
+        listitem.remove();
+
+    }
+    
 });
